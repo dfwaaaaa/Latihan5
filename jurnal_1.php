b8851c7ece0f2107be8028e39a592788e777e19c
dfwaaaaa <aufaabu483@gmail.com>
2024-10-30 17:31:50 +0700

    webpro5

---
+++ jurnal_1.php
@@ -0,0 +1,33 @@
+<?php
+echo"[1] Rata-rata \n";
+echo"[2] Penjumlahan \n";
+echo"[3] Rata-rata & Penjumlahan \n";
+echo"[Lainnya] Keluar \n";
+
+$pilihan = (int)readline("Masukkan pilihan disini: ");
+$total = 0.0; 
+$jumlah_angka = 0;
+$angka = 1; 
+
+# Looping input angka 
+while ($angka >= 0){ // inisialisasi angka positif
+    $angka = (float)readline("Masukkan angka: ");
+    if ($angka < 0){ // inisialisasi angka negatif
+        break;
+    }
+    $total += $angka; // Buat tambah
+    $jumlah_angka++; // Jumlah angka di tambah 1
+}
+
+if ($pilihan == 1) {
+    $hasil = $total / $jumlah_angka; 
+    echo "Hasil Rata-rata: " .$hasil;
+} else if ($pilihan == 2) {
+    echo "Hasil Penjumlahan" .$total;
+} else if ($pilihan == 3) {
+    $hasil = $total / $jumlah_angka;
+    echo "Hasil untuk rata-rata adalah " . $hasil . "\nHasil untuk penjumlahan adalah " . $total;
+} else {
+    echo "Keluar";
+}
+?>
